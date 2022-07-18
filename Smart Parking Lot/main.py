eb94fca408bc1e55aa57e98f6908f868ba90ea55
ekoboe <109064310+ekoboe@users.noreply.github.com>
2022-07-18 11:58:23 +0700

    Update main.py

--- Smart Parking Lot/main.py
+++ Smart Parking Lot/main.py
@@ -66,6 +66,10 @@ GPIO.output(21, GPIO.LOW)
 ###############################################################
 
 @app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/login")
 def login():
     return render_template('login.html')
 
